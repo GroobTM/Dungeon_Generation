9254d9bdf98e6c636989a2e1c54533c7a4375c6f
Reuben Wiles Maguire <reubenwilesmaguire@hotmail.co.uk>
2026-06-15 19:40:08 +0100

    Created room cell prefab and script

---
+++ Assets/Scripts/DungeonGeneratorRoomCell.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGeneratorRoomCell : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject northWall;
+    [SerializeField]
+    private GameObject eastWall;
+    [SerializeField]
+    private GameObject southWall;
+    [SerializeField]
+    private GameObject westWall;
+
+    [SerializeField]
+    private List<GameObject> northDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> eastDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> southDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> westDetails = new List<GameObject>();
+
+    [SerializeField]
+    private List<GameObject> northEastDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> southEastDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> southWestDetails = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> northWestDetails = new List<GameObject>();
+
+    private void Start()
+    {
+        northWall.SetActive(false);
+        eastWall.SetActive(false);
+        southWall.SetActive(false);
+        westWall.SetActive(false);
+
+        northDetails.ForEach(obj  => obj.SetActive(false));
+        eastDetails.ForEach(obj => obj.SetActive(false));
+        southDetails.ForEach(obj => obj.SetActive(false));
+        westDetails.ForEach(obj => obj.SetActive(false));
+
+        northEastDetails.ForEach(obj => obj.SetActive(false));
+        southEastDetails.ForEach(obj => obj.SetActive(false));
+        southWestDetails.ForEach(obj => obj.SetActive(false));
+        northWestDetails.ForEach(obj => obj.SetActive(false));
+    }
+
+    public void Configure(CellOptions cellOptions)
+    {
+        bool north = (cellOptions & CellOptions.NORTH) != 0;
+        bool east = (cellOptions & CellOptions.EAST) != 0;
+        bool south = (cellOptions & CellOptions.SOUTH) != 0;
+        bool west = (cellOptions & CellOptions.WEST) != 0;
+
+
+        if (north)
+        {
+            northWall.SetActive(true);
+            northDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (east)
+        {
+            eastWall.SetActive(true);
+            eastDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (south)
+        {
+            southWall.SetActive(true);
+            southDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (west)
+        {
+            westWall.SetActive(true);
+            westDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (north && east)
+        {
+            northEastDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (south && east)
+        {
+            southEastDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (south && west)
+        {
+            southWestDetails.ForEach(obj => obj.SetActive(true));
+        }
+
+        if (north && west)
+        {
+            northWestDetails.ForEach(obj => obj.SetActive(true));
+        }
+    }
+
+    [Flags]
+    public enum CellOptions
+    {
+        NONE    = 0,
+        NORTH   = 1 << 0,
+        EAST    = 1 << 1,
+        SOUTH   = 1 << 2,
+        WEST    = 1 << 3,
+    }
+}
